58b7c72e76709e4a5d19da3759286d0f762a1958
Arif Zamri <mm.arif.mz@gmail.com>
2026-09-04 16:18:02 +0800

    Apply canonical import formatting

--- src/lakehouse_rescue/transformations.py
+++ src/lakehouse_rescue/transformations.py
@@ -1,7 +1,7 @@
 """Pure PySpark transformations used by the Databricks job and unit tests."""
 
-from pyspark.sql import DataFrame, Window, functions as F
-
+from pyspark.sql import DataFrame, Window
+from pyspark.sql import functions as F
 
 REQUIRED_COLUMNS = {"order_id", "customer_id", "customer_name", "channel", "amount", "event_ts", "ingested_at"}
 
